5a4df70c992214ccf49fd851e5b740470e3914c7
Guilherme <guilhermereis139@gmail.com>
2022-03-02 21:14:47 -0300

    ok

---
+++ exercicios.facec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicios.facec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
