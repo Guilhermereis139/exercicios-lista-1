5c810c0bbc363b7988872c8d3dd57deeabfcbd63
Guilherme <guilhermereis139@gmail.com>
2022-03-02 22:31:06 -0300

    ok

---
+++ Exercicio.dois/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio.dois
+{
+    class Program
+    {
+        private static readonly decimal valorCambioStatic = 5.22m;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor em $: ");
+            string valorDigitado = Console.ReadLine();
+            decimal valorConvertido = 0.0m;
+            decimal valorCambio = 5.22m;
+
+            decimal.TryParse(valorDigitado, out valorConvertido);
+
+            decimal valorCambioConvertido = valorConvertido = valorCambio;
+
+            Console.WriteLine("$*valor em R$:  (valorConvertido)");
+            Console.ReadLine();
+        }
+    }
+}
